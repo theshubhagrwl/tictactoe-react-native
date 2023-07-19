74163c50ab797cc5643a1ef74f42cf8eeae610b6
theshubhagrwl <shubhagrawal4457@gmail.com>
2023-07-19 17:22:44 +0530

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+import Snackbar from 'react-native-snackbar';
+import Icons from './components/Icons';
+
+function App(): JSX.Element {
+  const [isCross, setIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setGameWinner('');
+    setGameState(new Array(9).fill('empty', 0, 9));
+  };
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Tic Tac Toe</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
